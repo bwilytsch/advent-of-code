84fb6983ff67f3a9dfb43f28c9011d8ac1465383
Bojan Wilytsch <bwilytsch@gmail.com>
2024-10-31 12:14:28 +0100

    add benchmarking to go codebase

--- go/2019/day-1/day_1_test.go
+++ go/2019/day-1/day_1_test.go
@@ -10,17 +10,25 @@ import (
 func TestPartOne(t *testing.T) {
 	assert.Equal(t, partOne("1969"), 654)
 	assert.Equal(t, partOne("100756"), 33583)
-
-	fileInput, _ := os.ReadFile("./input.txt")
-
-	println(partOne(string(fileInput)))
 }
 
 func TestPartTwo(t *testing.T) {
 	assert.Equal(t, partTwo("1969"), 966)
 	assert.Equal(t, partTwo("100756"), 50346)
+}
 
+func BenchmarkPartOne(b *testing.B) {
 	fileInput, _ := os.ReadFile("./input.txt")
 
-	println(partOne(string(fileInput)))
+	for i := 0; i < b.N; i++ {
+		partOne(string(fileInput))
+	}
+}
+
+func BenchmarkPartTwo(b *testing.B) {
+	fileInput, _ := os.ReadFile("./input.txt")
+
+	for i := 0; i < b.N; i++ {
+		partTwo(string(fileInput))
+	}
 }
---
+++ go/README.md
@@ -0,0 +1,19 @@
+# Run
+Fill in some stuff here
+
+# Benchmark
+```bash
+$ go test -bench <dir> -benchmem
+```
+
+## Write to file
+```bash
+$ go test -bench <dir> -benchmem > <file.txt>
+```
+
+## View benchmarks
+
+```bash
+$ benchstat <file_1> <file_2>
+```
+
