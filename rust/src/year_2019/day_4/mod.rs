f7eecb3f203229fc271383553e958f03bad1dd52
bwilytsch <bwilytsch@gmail.com>
2024-11-01 23:39:25 +0100

    rust day 4

---
+++ rust/src/year_2019/day_4/mod.rs
@@ -0,0 +1,120 @@
+use anyhow::Result;
+
+// Maybe bit shifting to find doubles works here
+fn check(num: &str) -> bool {
+    let mut prev = 0;
+    let mut double = false;
+
+    for n in num.chars().filter_map(|c| c.to_digit(10)) {
+        if n < prev {
+            return false;
+        }
+
+        if prev == n {
+            double = true;
+        }
+
+        prev = n;
+    }
+
+    if !double {
+        return false;
+    }
+
+    true
+}
+
+fn check_thorough(num: &str) -> bool {
+    let mut prev = 0;
+    let mut doubled = false;
+    let mut collector: Vec<u32> = vec![];
+
+    // needs a pair, groups of 2 or more don't count
+
+    for n in num.chars().filter_map(|c| c.to_digit(10)) {
+        if n < prev {
+            return false;
+        }
+
+        if let Some(a) = collector.last() {
+            if a != &n {
+                if collector.len() == 2 {
+                    doubled = true;
+                }
+
+                collector.clear();
+            }
+
+            collector.push(n);
+        } else {
+            collector.push(n);
+        }
+
+        prev = n;
+    }
+
+    doubled || collector.len() == 2
+}
+
+fn part_one(input: &str) -> Result<i32> {
+    let ranges = input
+        .split('-')
+        .map(|sub| sub.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    let mut count = 0;
+
+    if let (Some(start), Some(end)) = (ranges.first(), ranges.get(1)) {
+        for pw in *start..=*end {
+            if check(&pw.to_string()) {
+                count += 1;
+            }
+        }
+    }
+
+    Ok(count)
+}
+
+fn part_two(input: &str) -> Result<i32> {
+    let mut count = 0;
+    let ranges = input
+        .split('-')
+        .map(|sub| sub.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    if let (Some(start), Some(end)) = (ranges.first(), ranges.get(1)) {
+        for pw in *start..=*end {
+            if check_thorough(&pw.to_string()) {
+                count += 1;
+            }
+        }
+    }
+
+    Ok(count)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one() -> Result<()> {
+        assert!(check("111111"));
+        assert!(!check("223450"));
+        assert!(!check("123789"));
+
+        println!("{}", part_one("347312-805915")?);
+
+        Ok(())
+    }
+
+    #[test]
+    fn two() -> Result<()> {
+        assert!(check_thorough("112233"));
+        assert!(!check_thorough("123444"));
+        assert!(check_thorough("111122"));
+
+        println!("{}", part_two("347312-805915")?);
+
+        Ok(())
+    }
+}
