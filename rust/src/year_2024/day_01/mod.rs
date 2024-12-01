beebd94e0d746e1eaca7c4d8b394683449933844
Bojan Wilytsch <bwilytsch@gmail.com>
2024-12-02 08:35:40 +0900

    day 01 - part 1

--- rust/src/year_2024/day_01/mod.rs
+++ rust/src/year_2024/day_01/mod.rs
@@ -1,21 +1,53 @@
 use anyhow::Result;
 
-pub fn part_one(input: &str) -> Result<String> {
-    println!("{}", input);
-    Ok(String::from(input))
+pub fn part_one(input: &str) -> Result<i32> {
+    let mut columns: (Vec<&str>, Vec<&str>) = input
+        .lines()
+        .map(|line| line.trim().split_once(' ').unwrap())
+        .unzip();
+
+    columns.0.sort();
+    columns.1.sort();
+
+    let mut sum = 0;
+
+    // Assuming they are of equal length
+    for (index, entry) in columns.0.iter().enumerate() {
+        let value = entry.parse::<i32>().unwrap();
+        let other = columns.1.get(index).unwrap().trim().parse::<i32>().unwrap();
+        let delta = (value - other).abs();
+        sum += delta;
+    }
+
+    Ok(sum)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use super::*;
 
     #[test]
     fn one() -> Result<()> {
-        todo!()
+        let test = "3   4
+                    4   3
+                    2   5
+                    1   3
+                    3   9
+                    3   3";
+
+        assert_eq!(part_one(test)?, 11);
+
+        if let Ok(file_input) = fs::read_to_string("./inputs/2024/001/input.txt") {
+            println!("{}", part_one(&file_input)?);
+        }
+
+        Ok(())
     }
 
-    #[test]
-    fn two() -> Result<()> {
-        todo!()
-    }
+    // #[test]
+    // fn two() -> Result<()> {
+    //     todo!()
+    // }
 }
