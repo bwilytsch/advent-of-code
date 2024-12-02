edf462012722d672bb2bb0ce8bd6a9f2171409a9
Bojan Wilytsch <bwilytsch@gmail.com>
2024-12-02 15:32:47 +0900

    day 01 - part 2

--- rust/src/year_2024/day_01/mod.rs
+++ rust/src/year_2024/day_01/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 
 pub fn part_one(input: &str) -> Result<i32> {
@@ -18,6 +20,46 @@ pub fn part_one(input: &str) -> Result<i32> {
         let delta = (value - other).abs();
         sum += delta;
     }
+
+    Ok(sum)
+}
+
+pub fn part_two(input: &str) -> Result<i32> {
+    let columns: (Vec<&str>, Vec<&str>) = input
+        .lines()
+        .map(|line| line.trim().split_once(' ').unwrap())
+        .unzip();
+
+    let mut lot: HashMap<i32, i32> = HashMap::new();
+
+    for entry in columns
+        .1
+        .iter()
+        .map(|value| value.trim().parse::<i32>().unwrap())
+    {
+        match lot.get(&entry) {
+            Some(count) => {
+                lot.insert(entry, count + 1);
+            }
+            None => {
+                lot.insert(entry, 1);
+            }
+        }
+    }
+
+    // Assuming they are of equal length
+    let sum = columns
+        .0
+        .iter()
+        .map(|n| {
+            let num = n.parse::<i32>().unwrap();
+            if let Some(value) = lot.get(&num) {
+                return num * value;
+            }
+
+            0
+        })
+        .sum();
 
     Ok(sum)
 }
@@ -46,8 +88,21 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn two() -> Result<()> {
-    //     todo!()
-    // }
+    #[test]
+    fn two() -> Result<()> {
+        let test = "3   4
+                    4   3
+                    2   5
+                    1   3
+                    3   9
+                    3   3";
+
+        assert_eq!(part_two(test)?, 31);
+
+        if let Ok(file_input) = fs::read_to_string("./inputs/2024/001/input.txt") {
+            println!("{}", part_two(&file_input)?);
+        }
+
+        Ok(())
+    }
 }
