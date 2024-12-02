dcf0fd06831d5ab845e68d2d6ed2bb37bebfc2a8
Bojan Wilytsch <bwilytsch@gmail.com>
2024-12-02 20:10:31 +0900

    day 02 - part 2
    - With cleanup for part 1

--- rust/src/year_2024/day_02/mod.rs
+++ rust/src/year_2024/day_02/mod.rs
@@ -1,50 +1,55 @@
 use anyhow::Result;
 
-pub fn part_one(input: &str) -> Result<i32> {
-    let valid_reports = input
-        .lines()
-        .filter_map(|report| {
-            let levels = report
-                .split_whitespace()
-                .filter_map(|level| level.parse::<i32>().ok())
-                .collect::<Vec<i32>>();
-
-            let mut prev_dir = 0;
-
-            if levels.windows(2).all(|v| {
-                let a = v[0];
-                let b = v[1];
-
-                let mut delta = a - b;
-
-                let current_dir = match delta < 0 {
-                    true => -1,
-                    false => 1,
-                };
-
-                if prev_dir != 0 && prev_dir != current_dir {
-                    return false;
-                };
-
-                prev_dir = current_dir;
-
-                delta = delta.abs();
-
-                !(delta < 1 || delta > 3)
-            }) {
-                return Some(1);
-            }
-
-            None
-        })
-        .sum();
-
-    Ok(valid_reports)
+fn parse_input(input: &str) -> Vec<i32> {
+    input
+        .split_whitespace()
+        .filter_map(|level| level.parse::<i32>().ok())
+        .collect::<Vec<i32>>()
 }
 
-// pub fn part_two(input: &str) -> Result<i32> {
-//     Ok(0)
-// }
+fn is_safe(levels: &Vec<i32>) -> Option<i32> {
+    match levels.windows(2).all(|v| (1..=3).contains(&(v[0] - v[1])))
+        || levels.windows(2).all(|v| (1..=3).contains(&(v[1] - v[0])))
+    {
+        true => Some(0),
+        false => None,
+    }
+}
+
+pub fn part_one(input: &str) -> Result<usize> {
+    Ok(input
+        .lines()
+        .filter_map(|report| is_safe(&parse_input(&report)))
+        .count())
+}
+
+pub fn part_two(input: &str) -> Result<usize> {
+    Ok(input
+        .lines()
+        .filter_map(|report| {
+            let levels = parse_input(&report);
+
+            if is_safe(&levels).is_some() {
+                return Some(1);
+            } else {
+                // This is a brute force method
+                match (0..levels.len())
+                    .map(|i| {
+                        levels
+                            .iter()
+                            .enumerate()
+                            .filter_map(|(idx, &value)| if i != idx { Some(value) } else { None })
+                            .collect()
+                    })
+                    .any(|abridged_level| is_safe(&abridged_level).is_some())
+                {
+                    true => Some(1),
+                    false => None,
+                }
+            }
+        })
+        .count())
+}
 
 #[cfg(test)]
 mod tests {
@@ -70,7 +75,21 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn two() -> Result<()> {
-    // }
+    #[test]
+    fn two() -> Result<()> {
+        let example = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+        assert_eq!(part_two(example)?, 4);
+
+        if let Ok(file_input) = fs::read_to_string("./inputs/2024/002/input.txt") {
+            println!("{}", part_two(&file_input)?);
+        }
+
+        Ok(())
+    }
 }
