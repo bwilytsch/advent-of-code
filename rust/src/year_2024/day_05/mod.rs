7fd77fd24e8da7b000d0cbb4035aa03d7c7bd7e5
Bojan Wilytsch <bwilytsch@gmail.com>
2024-12-06 20:52:21 +0900

    [Rust] Day 5 - part 2

--- rust/src/year_2024/day_05/mod.rs
+++ rust/src/year_2024/day_05/mod.rs
@@ -2,6 +2,34 @@ use std::collections::{HashMap, HashSet};
 
 use anyhow::Result;
 
+fn bubble_sort(pages: &Vec<&str>, rules: &HashMap<&str, HashSet<&str>>) -> Option<i32> {
+    let mut v = pages.clone();
+    let mut swapped = true;
+
+    while swapped {
+        swapped = false;
+        for i in 0..v.len() - 1 {
+            let a = v[i];
+            let b = v[i + 1];
+
+            if let Some(rule) = rules.get(b) {
+                if rule.contains(a) {
+                    // If b should come before a, swap them
+                    v.swap(i, i + 1);
+                    swapped = true;
+                }
+            }
+        }
+    }
+
+    if &v == pages {
+        return None;
+    }
+
+    let middle = v[v.len() / 2];
+    middle.parse::<i32>().ok()
+}
+
 fn is_valid(pages: &Vec<&str>, rules: &HashMap<&str, HashSet<&str>>) -> bool {
     for (i, p1) in pages.iter().enumerate() {
         for j in 0..i {
@@ -64,9 +92,38 @@ pub fn part_one(input: &str) -> Result<i32> {
     Ok(sum)
 }
 
-// pub fn part_two(input: &str) -> Result<()> {
-//     Ok(())
-// }
+pub fn part_two(input: &str) -> Result<i32> {
+    let instructions = input.split("\n\n").collect::<Vec<&str>>();
+    let (rules, pages) = (instructions[0], instructions[1]);
+
+    let rules: HashMap<&str, HashSet<&str>> =
+        rules.lines().fold(HashMap::new(), |mut map, line| {
+            let mut p = line.split('|');
+
+            let key = p.next().unwrap();
+            let value = p.next().unwrap();
+
+            let record = map.entry(key).or_insert({
+                let mut set = HashSet::new();
+                set.insert(value);
+                set
+            });
+
+            record.insert(value);
+
+            map
+        });
+
+    let sum = pages
+        .lines()
+        .filter_map(|line| {
+            let pages = line.split(",").collect::<Vec<&str>>();
+            bubble_sort(&pages, &rules)
+        })
+        .sum::<i32>();
+
+    Ok(sum)
+}
 
 #[cfg(test)]
 mod tests {
@@ -87,8 +144,17 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn two() -> Result<()> {
-    //     Ok(())
-    // }
+    #[test]
+    fn two() -> Result<()> {
+        if let Ok(input) = fs::read_to_string("./inputs/2024/005/example.txt") {
+            assert_eq!(part_two(&input)?, 123);
+        };
+
+        // 7730 too high
+        if let Ok(input) = fs::read_to_string("./inputs/2024/005/input.txt") {
+            println!("{}", part_two(&input)?);
+        };
+
+        Ok(())
+    }
 }
