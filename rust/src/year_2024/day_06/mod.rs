7d9eacd825ba6b053f7d20ada723be74232ab14f
Bojan Wilytsch <bwilytsch@gmail.com>
2024-12-08 00:19:59 +0900

    [Rust] Day 6 - part 2

--- rust/src/year_2024/day_06/mod.rs
+++ rust/src/year_2024/day_06/mod.rs
@@ -1,10 +1,16 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use anyhow::Result;
 
 struct Grid {
     columns: i32,
     rows: i32,
+}
+
+#[derive(Eq, PartialEq, Hash, Debug)]
+pub struct PathNode {
+    coord: i32,
+    dir: usize,
 }
 
 fn index_to_position(idx: &i32, grid: &Grid) -> (i32, i32) {
@@ -24,6 +30,10 @@ fn move_to(idx: &i32, x: &i32, y: &i32, grid: &Grid) -> i32 {
     position_to_index(&new_x, &new_y, grid)
 }
 
+fn is_inbounds(p0: &Point, grid: &Grid) -> bool {
+    p0.x >= 0 && p0.y >= 0 && p0.x < grid.columns && p0.y < grid.rows
+}
+
 fn inside_bounds(idx: &i32, grid: &Grid) -> bool {
     let (x, y) = index_to_position(idx, grid);
     x >= 0 && y >= 0 && x < grid.columns && y < grid.rows
@@ -38,8 +48,8 @@ pub fn part_one(input: &str) -> Result<usize> {
     let rows = input.lines().count() as i32;
     let columns = input.lines().map(|l| l.len()).max().unwrap() as i32;
     let grid = Grid { columns, rows };
-
     let mut visited: HashSet<i32> = HashSet::new();
+
     let mut guard = chars
         .iter()
         .enumerate()
@@ -72,8 +82,211 @@ pub fn part_one(input: &str) -> Result<usize> {
     Ok(visited.len())
 }
 
-// pub fn part_two(input: &str) -> Result<i32> {
-// }
+fn print_path(chars: &Vec<char>, grid: &Grid) {
+    for i in 0..grid.rows {
+        let slice = chars[((i * grid.columns) as usize)..(((i + 1) * grid.columns) as usize)]
+            .into_iter()
+            .collect::<String>();
+
+        println!("{:?}", slice);
+    }
+
+    println!("")
+}
+
+#[derive(Eq, PartialEq, Debug, Copy, Clone, Hash)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Point { x, y }
+    }
+
+    fn from_index(idx: &i32, grid: &Grid) -> Self {
+        let (x, y) = index_to_position(idx, grid);
+        Point::new(x, y)
+    }
+
+    fn add_point(&mut self, p0: Point) -> Self {
+        Point::new(self.x + p0.x, self.y + p0.y)
+    }
+}
+
+// NOTE: This is only unique with a hashset, I need to be able to properly dedpulicate my points
+fn explore(start: &i32, start_dir: &usize, grid: &Grid, chars: &Vec<char>) -> Vec<(Point, usize)> {
+    let mut adjusted_chars = chars.clone();
+    let mut unique: HashSet<Point> = HashSet::new();
+    let mut visited: Vec<(Point, usize)> = Vec::new();
+    let mut dir = start_dir.clone();
+    let dirs = vec![
+        Point::new(0, -1),
+        Point::new(1, 0),
+        Point::new(0, 1),
+        Point::new(-1, 0),
+    ];
+
+    let mut pos = Point::from_index(start, grid);
+
+    while is_inbounds(&pos, &grid) {
+        if !unique.contains(&pos) {
+            visited.push((pos, dir));
+            unique.insert(pos);
+        }
+
+        let current_idx = position_to_index(&pos.x, &pos.y, grid);
+        adjusted_chars[current_idx as usize] = 'X';
+
+        let heading = dirs[dir];
+        let new_position = pos.add_point(heading);
+        let next = position_to_index(&new_position.x, &new_position.y, grid);
+
+        match adjusted_chars.get(next as usize) {
+            Some(c) if *c == '#' => {
+                dir = (dir + 1) % dirs.len();
+                continue;
+            }
+            _ => {
+                pos = new_position;
+            }
+        }
+    }
+
+    visited
+}
+
+fn check_loop(
+    start: &Point,
+    cache: &Vec<Point>,
+    cur: &Point,
+    start_dir: &usize,
+    grid: &Grid,
+    chars: &Vec<char>,
+) -> bool {
+    let mut pos = start.clone();
+
+    let mut adjusted_chars = chars.clone();
+    let mut visited: Vec<Point> = cache.clone();
+
+    let mut collisions: HashMap<Point, usize> = HashMap::new();
+
+    // Backfill other items for viz
+    for p in visited.iter() {
+        let pos_index = position_to_index(&p.x, &p.y, grid);
+        adjusted_chars[pos_index as usize] = 'X';
+    }
+
+    // print_path(&adjusted_chars, grid);
+
+    let mut dir = start_dir.clone();
+    let dirs = vec![
+        Point::new(0, -1),
+        Point::new(1, 0),
+        Point::new(0, 1),
+        Point::new(-1, 0),
+    ];
+
+    if is_inbounds(&cur, grid) {
+        let idx = position_to_index(&cur.x, &cur.y, grid) as usize;
+        adjusted_chars[idx] = 'O';
+    }
+
+    let mut counter = 0;
+
+    while is_inbounds(&pos, &grid) {
+        visited.push(pos);
+        let pos_index = position_to_index(&pos.x, &pos.y, grid);
+        adjusted_chars[pos_index as usize] = 'X';
+
+        let heading = dirs[dir];
+        let new_position = pos.add_point(heading);
+        let next = position_to_index(&new_position.x, &new_position.y, grid);
+
+        match adjusted_chars.get(next as usize) {
+            Some(c) if *c == 'O' => {
+                dir = (dir + 1) % dirs.len();
+
+                counter += 1;
+
+                if counter > 1 {
+                    return true;
+                }
+
+                continue;
+            }
+            Some(c) if *c == '#' => {
+                let entry = collisions.entry(pos).or_insert(0);
+                *entry += 1;
+
+                if *entry > 1 {
+                    return true;
+                }
+
+                dir = (dir + 1) % dirs.len();
+
+                continue;
+            }
+            _ => {
+                pos = new_position;
+            }
+        }
+    }
+
+    false
+}
+
+pub fn part_two(input: &str) -> Result<usize> {
+    // Assming it always starts going up with `^`
+    let chars = input.chars().filter(|c| *c != '\n').collect::<Vec<char>>();
+    let forward_dir = 0;
+    let mut obstacles: HashSet<(i32, i32)> = HashSet::new();
+
+    let rows = input.lines().count() as i32;
+    let columns = input.lines().map(|l| l.len()).max().unwrap() as i32;
+    let grid = Grid { columns, rows };
+
+    let guard = chars
+        .iter()
+        .enumerate()
+        .filter_map(|(i, c)| {
+            if *c == '^' {
+                return Some(i);
+            }
+
+            if *c == '#' {
+                let pos = index_to_position(&(i as i32), &grid);
+                obstacles.insert(pos);
+            }
+
+            None
+        })
+        .next()
+        .unwrap() as i32;
+
+    let solved_map = explore(&guard, &forward_dir, &grid, &chars);
+
+    println!("{}", solved_map.len());
+
+    let mut count = 0;
+    let mut cache = vec![];
+    let mut prev = solved_map.clone().into_iter().next().unwrap().0;
+
+    for (cur, dir) in solved_map.iter().skip(1) {
+        let next = cur.clone();
+        cache.push(next);
+
+        if check_loop(&prev, &cache, &cur, &dir, &grid, &chars) {
+            // println!("{count}");
+            count += 1;
+        }
+
+        prev = next;
+    }
+
+    Ok(count)
+}
 
 #[cfg(test)]
 mod tests {
@@ -94,17 +307,16 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn two() -> Result<()> {
-    //     if let Ok(input) = fs::read_to_string("./inputs/2024/005/example.txt") {
-    //         assert_eq!(part_two(&input)?, 123);
-    //     };
-    //
-    //     // 7730 too high
-    //     if let Ok(input) = fs::read_to_string("./inputs/2024/005/input.txt") {
-    //         println!("{}", part_two(&input)?);
-    //     };
-    //
-    //     Ok(())
-    // }
+    #[test]
+    fn two() -> Result<()> {
+        if let Ok(input) = fs::read_to_string("./inputs/2024/006/example.txt") {
+            assert_eq!(part_two(&input)?, 6);
+        };
+
+        if let Ok(input) = fs::read_to_string("./inputs/2024/006/input.txt") {
+            println!("{}", part_two(&input)?);
+        }
+
+        Ok(())
+    }
 }
