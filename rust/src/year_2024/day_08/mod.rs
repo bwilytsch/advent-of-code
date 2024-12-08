42460ef18c90032b96c51c4e2dca18fa2b2b9abb
Bojan Wilytsch <bwilytsch@gmail.com>
2024-12-08 16:25:39 +0100

    [Rust] Day 8 - part 2

--- rust/src/year_2024/day_08/mod.rs
+++ rust/src/year_2024/day_08/mod.rs
@@ -47,7 +47,7 @@ struct Grid {
 }
 
 fn in_bounds(p: &Point, grid: &Grid) -> bool {
-    p.x < 0 || p.y < 0 || p.x > (grid.columns as i32) - 1 || p.y > (grid.rows as i32) - 1
+    p.x >= 0 && p.y >= 0 && p.x < (grid.columns as i32) && p.y < (grid.rows as i32)
 }
 
 pub fn part_one(input: &str) -> Result<usize> {
@@ -75,34 +75,77 @@ pub fn part_one(input: &str) -> Result<usize> {
     let mut unique_antinodes: HashSet<Point> = HashSet::new();
 
     for (_, points) in lot {
-        for (i, p0) in points.iter().enumerate() {
+        for p0 in points.iter() {
             for j in 0..points.len() {
                 let p1 = points[j];
 
-                // These are sorted due to the way we are parsing the input
-                // if p0.x.abs_diff(p1.x) >= 2 || p0.y.abs_diff(p1.y) >= 2 {
                 let d = p0.distance(&p1);
 
                 let values = vec![p0.sub(&d), p1.add(&d), p0.add(&d), p1.sub(&d)];
 
-                values
-                    .iter()
-                    .filter(|p| !in_bounds(p, &grid))
-                    .for_each(|p| {
-                        if p != p0 && p != &p1 {
-                            unique_antinodes.insert(p.clone());
-                        }
-                    });
+                values.iter().filter(|p| in_bounds(p, &grid)).for_each(|p| {
+                    if p != p0 && p != &p1 {
+                        unique_antinodes.insert(p.clone());
+                    }
+                });
             }
         }
     }
-    println!("{:?}", unique_antinodes);
 
     Ok(unique_antinodes.iter().count())
 }
 
-pub fn part_two(input: &str) -> Result<i64> {
-    Ok(0)
+pub fn part_two(input: &str) -> Result<usize> {
+    let rows = input.lines().collect::<Vec<_>>();
+    let columns = rows[0].len();
+    let grid = Grid {
+        rows: rows.len(),
+        columns,
+    };
+
+    let mut lot: HashMap<char, Vec<Point>> = HashMap::new();
+
+    for (row, line) in input.lines().enumerate() {
+        for (column, c) in line.chars().enumerate() {
+            if c.is_alphanumeric() {
+                let entry = lot.entry(c).or_insert(vec![]);
+                entry.push(Point {
+                    x: column as i32,
+                    y: row as i32,
+                })
+            }
+        }
+    }
+
+    let mut unique_antinodes: HashSet<Point> = HashSet::new();
+
+    for (_, points) in lot {
+        for p0 in points.iter() {
+            for j in 0..points.len() {
+                let p1 = points[j];
+
+                if p0 == &p1 {
+                    continue;
+                }
+
+                let d = p0.distance(&p1);
+
+                let mut cur = p0.clone();
+                while in_bounds(&cur, &grid) {
+                    unique_antinodes.insert(cur.clone());
+                    cur = cur.add(&d);
+                }
+
+                let mut cur = p0.clone();
+                while in_bounds(&cur, &grid) {
+                    unique_antinodes.insert(cur.clone());
+                    cur = cur.sub(&d);
+                }
+            }
+        }
+    }
+
+    Ok(unique_antinodes.iter().count())
 }
 
 #[cfg(test)]
@@ -126,11 +169,11 @@ mod tests {
 
     #[test]
     fn two() -> Result<()> {
-        if let Ok(input) = fs::read_to_string("./inputs/2024/007/example.txt") {
+        if let Ok(input) = fs::read_to_string("./inputs/2024/008/example.txt") {
             assert_eq!(part_two(&input)?, 34);
         };
 
-        if let Ok(input) = fs::read_to_string("./inputs/2024/007/input.txt") {
+        if let Ok(input) = fs::read_to_string("./inputs/2024/008/input.txt") {
             println!("{}", part_two(&input)?);
         }
 
